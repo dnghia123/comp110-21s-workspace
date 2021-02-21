1f9a1ed95f752331f0edd033139b784c2d7a4319
dnghia123 <nnghia@email.unc.edu>
2021-02-21 15:37:43 -0500

    Progress on Exercise 2

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -12,20 +12,20 @@ def main() -> None:
     doses: int = int(input("Doses administered: "))
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
-    days: int = days_to_target(population,doses,doses_per_day,target)
+    days: int = days_to_target(population, doses, doses_per_day, target)
     date: str = future_date(days)
     print("We will reach " + str(target) + "% vaccination in " + str(days) + " days which falls on " + str(date)) 
 
 
 def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
-    """Days until target is reached"""
+    """Days until target is reached."""
     Doses_target_goal:int = int(((target/100) * population) - (doses/2)) 
     Days_till_target_goal: int = int((Doses_target_goal * 2) / (doses_per_day)) 
     return Days_till_target_goal 
 
 
 def future_date(days: int) -> str: 
-    """Date of target"""
+    """Date of target."""
     Days_till_goal: timedelta = timedelta(days)
     today: datetime = datetime.today()
     Date_of_goal: datetime = today + Days_till_goal 
