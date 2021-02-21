30a4f8719d4d5ad61d778848d6e84b06f4039e89
dnghia123 <nnghia@email.unc.edu>
2021-02-21 15:28:07 -0500

    Progress on Exercise 2

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -1,7 +1,7 @@
 """Vaccine calculator exercise as a structured program."""
 
 from datetime import datetime, timedelta
-from exercises.ex01.vaccine_calc import Days_till_target_goal
+
 
 __author__ = "730410603"
 
@@ -14,19 +14,19 @@ def main() -> None:
     target: int = int(input("Target percent vaccinated: "))
     days: int = days_to_target(population,doses,doses_per_day,target)
     date: str = future_date(days)
-    print("We will reach " + str(target) + "% vaccination in " + str(days) + " which falls on " + str(date)) 
+    print("We will reach " + str(target) + "% vaccination in " + str(days) + " days which falls on " + str(date)) 
 
 
-def days_to_target(population:int,doses:int,doses_per_day:int,target:int) -> int:
+def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
     """Days until target is reached"""
     Doses_target_goal:int = int(((target/100) * population) - (doses/2)) 
     Days_till_target_goal: int = int((Doses_target_goal * 2) / (doses_per_day)) 
     return Days_till_target_goal 
 
 
-def future_date(days:int) -> str: 
+def future_date(days: int) -> str: 
     """Date of target"""
-    Days_till_goal: timedelta =timedelta(days)
+    Days_till_goal: timedelta = timedelta(days)
     today: datetime = datetime.today()
     Date_of_goal: datetime = today + Days_till_goal 
     return Date_of_goal.strftime("%B %d, %Y")
