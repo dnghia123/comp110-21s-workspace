cfb7e44ff06ebc1cfdf8fe65dab2386fcaae6286
dnghia123 <nnghia@email.unc.edu>
2021-03-01 00:06:47 -0500

    Progress on Project 0

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -4,59 +4,63 @@ __author__ = "730410603"
 
 from random import randint
 
+player: str = ""
+points: int = 0
+
+
 def main() -> None:
    """Pet game procedure."""
-   player: str = str(input("Name your pet:"))
-   greet(player)
-   points: int = 0 
+   greet()
    print("Now, choose where you want to go for the day: \'Beach\', \'Park\', or \'Mountain\'. Type \'Exit\' to quit the game.")
    path: str = str(input("Choose where to go:"))
    if path == "Beach":
-       Beach(path,player,points)
+       Beach(path, player, points)
    else:
        if path == "Park":
-           Park(path,player,points) 
+           Park(path, player, points)
        else:
            if path == "Spa":
-               Spa(path,player,points)
+               Spa(path, player, points)
            else:
-               Exit(path,points) 
+               Exit(path, points) 
 
 
-def greet(player:str) -> None:
+def greet() -> None:
     """Game greeting."""
-    print(f"In this game, you will be taking care of a virtual pet. You can earn points based on the decisions made regarding your pet. The more points you earn, the stronger the bond between you and your pet. Your pet name is:{player}")
+    player = str(input("Name your pet: "))
+    print(f"You will be taking care of a virtual pet. Your pet name is: {player}")
 
 
-def Beach(path:str, player:str, points:int) -> None:
+def Beach(path: str, player: str, points: int) -> None:
     """Trip to the beach."""
     while input(f"Do you want to play fench with {player}? yes/no - ") == "yes":
         points += 1
         print(f"You earn {points} points.")
-    Exit(path,points)
+    Exit(path, points)
 
 
+def Park(path:str, player:str, points:int) -> int:
+    """Trip to the park.""" 
+    while input(f"Do you wanna look for tennis balls with {player}? yes/no - ") == "yes":
+        balls: int = randint(1,25)
+        points += balls
+        print(f"Wow, {player} collected {balls} balls")
+    Exit(path, points)
+    return points
 
-def Park(path:str, player:str, points:int) -> None:
-    """Trip to the park."""
-    points = 0 
-    while input(f"Do you to rub {player}\'s belly? yes/no - ") == "yes":
-        points += 1
-        print(f"You earn {points} points.")
-    Exit(path,points)
 
-
-def Spa(path:str, player:str, points:int) -> None:
+def Spa(path: str, player: str, points: int) -> None:
     """Trip to the spa."""
     while input(f"Do you want to rub {player}\'s fur? yes/no -") == "yes":
         points += 1
         print(f"You earn {points} points.")
-    Exit(path,points)
+    Exit(path, points)
 
 
-def Exit(path:str,points:int) -> None:
+def Exit(path: str, points: int) -> None:
     """Quitting the game."""
     print(f"Thanks for playing the game, you earn {points} points.")
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":   
     main()
